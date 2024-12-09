599e6e890701e240a4867565cbbcd53d6300779f
Depanshu <122829642+Suya678@users.noreply.github.com>
2024-12-08 20:00:32 -0700

    Finished the routers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const PORT = 30000;
+const PORT = 3000;
 
 let circuits;
 let constructors;
@@ -20,11 +20,44 @@ try {
     process.exit(1);
 }
 
-// Validates that param is a number. Returns a number representation of param otherwise returns -1.
-function validateParamIsaNumber(param) {
+
+/**
+ * * Sends a response back with an error message indicating that the provided number is invalid.
+ *
+ * @param res  The response object
+ * @param number THe invalid number or value which will be embedded in the request
+ */
+function sendInvalidNumber(res, number) {
+    res.status(400).json({
+        error: "Invalid request",
+        message: `${number}' is not a valid number`});
+}
+
+/**
+ * * Sends a response back with an error message indicating that the requested resource does not exist
+ *
+ * @param res  The response object
+ * @param param THe resource that was requested
+ */
+function sendNoResultsFound(res, param) {
+    res.status(400).json({
+        error: "Invalid request",
+        message: `No results found for ${param}`});
+}
+
+
+
+/**
+ * Validates that the param is a number. If it is - returns the number as int ant other returns -1
+ *
+ * @param param The value being checked
+ * @returns -1 if the param is not an int, or it's int representation otherwise
+ */function validateParamIsaNumber(param) {
     const num = parseInt(param);
     return isNaN(num) ? -1 : num;
 }
+
+
 app.listen(
     PORT,
     ()=> console.log(`Server started on port http://localhost:${PORT}`)
@@ -38,15 +71,12 @@ app.get('/api/circuits', (req, res) => {
 
 // GET single circuit by circuitId
 app.get('/api/circuits/:id', (req, res) => {
-
     const id = validateParamIsaNumber(req.params.id);
     if(id === -1){
         return sendInvalidNumber(res, req.params.id);
     }
 
-    const circuit = circuits.find(c => {
-        return c.circuitId === req.params.id;
-    });
+    const circuit = circuits.find(c => c.circuitId === id);
     !circuit ? sendNoResultsFound(res, `id: ${req.params.id}`) :res.json(circuit);
 
 });
@@ -68,31 +98,33 @@ app.get('/api/constructorResults/:constructorRef/:year', (req, res) => {
     const constructor = constructors.find(c => c.constructorRef === req.params.constructorRef);
     console.log(constructor);
 
-    if(year === -1 || !constructor ) {
-        res.sendStatus(404);
-        return;
+    if(year === -1){
+        return sendInvalidNumber(res, req.params.year);
+    } else if(!constructor ) {
+        return sendNoResultsFound(res, `constructorRef: ${req.params.constructorRef}`);
     }
     const yearResultForConstructor = results.filter((result)=> {
         return result.constructor.ref === constructor.constructorRef && result.race.year === year;
     });
 
-   res.json(yearResultForConstructor);
+    if(yearResultForConstructor.length === 0) {
+        sendNoResultsFound(res, `constructorRef: ${ req.params.constructorRef} for driverRef: ${req.params.year}`);
+    } else {
+    res.json(yearResultForConstructor);
+
+    }
 });
 
 // GET all drivers
 app.get('/api/drivers', (req, res) => {
     res.json(drivers);
-
 });
 
 // GET single driver by driverRef
 app.get('/api/drivers/:ref', (req, res) => {
-    const driver = drivers.find(c => {
-        return c.driverRef === req.params.ref;
-    });
-    !driver ? res.sendStatus(404) : res.json(driver);
+    const driver = drivers.find(c => c.driverRef === req.params.ref);
+    !driver ? sendNoResultsFound(res, `driverRef: ${req.params.ref}`) : res.json(driver);
 });
-
 
 
 // GET race results for a driver for a specific season
@@ -100,25 +132,29 @@ app.get('/api/driverResults/:driverRef/:year', (req, res) => {
     const year = validateParamIsaNumber(req.params.year);
     const driver = drivers.find(c => c.driverRef === req.params.driverRef);
 
-    if(year === -1 || !driver ) {
-        res.sendStatus(404);
-        return;
+    if(year === -1){
+        return sendInvalidNumber(res, req.params.year);
+    } else if(!driver ) {
+        return sendNoResultsFound(res, `driverRef: ${req.params.driverRef}`);
     }
 
     const yearResultForDriver = results.filter((result)=> {
         return result.driver.ref === driver.driverRef && result.race.year === year;
     });
 
-    res.json(yearResultForDriver);
+    if(yearResultForDriver.length === 0) {
+        sendNoResultsFound(res, `driverRef: ${ req.params.driverRef} and  year: ${req.params.year}`);
+    } else {
+        res.json(yearResultForDriver);
+    }
+
 });
 
 // GET all races for a specific season
 app.get('/api/races/season/:year', (req, res) => {
     const year = validateParamIsaNumber(req.params.year);
-
     if(year === -1) {
-        res.sendStatus(404);
-        return;
+        return sendInvalidNumber(res, req.params.year);
     }
 
     const yearResults = results.filter((result)=> {
@@ -137,7 +173,11 @@ app.get('/api/races/id/:id', (req, res) => {
 
     const race = results.find(c => c.race.id === id);
     console.log(race);
-    !race ? res.sendStatus(404) : res.json(race);
+    if (!race) {
+        sendNoResultsFound(res, `id: ${req.params.id}`);
+    } else {
+        res.json(race);
+    }
 
 });
 
@@ -162,13 +202,13 @@ app.get('/api/results/race/:id', (req, res) => {
 app.get('/api/results/season/:year', (req, res) => {
     const year = validateParamIsaNumber(req.params.year);
 
-    if(year === -1){
+    if (year === -1) {
         return sendInvalidNumber(res, req.params.year);
     }
 
-    const allRaceResultsForTheYear = results.filter((result)=> result.race.year === year);
+    const allRaceResultsForTheYear = results.filter((result) => result.race.year === year);
 
-    if(allRaceResultsForTheYear.length === 0){
+    if (allRaceResultsForTheYear.length === 0) {
         sendNoResultsFound(res, `year: '${req.params.year}'`);
     } else {
         res.json(allRaceResultsForTheYear);
@@ -176,14 +216,8 @@ app.get('/api/results/season/:year', (req, res) => {
 
 });
 
-function sendInvalidNumber(res, number) {
-    res.status(400).json({
-        error: "Invalid request",
-        message: `Year parameter '${number}' is not a valid number`});
-}
 
-function sendNoResultsFound(res, param) {
-    res.status(400).json({
-        error: "Invalid request",
-        message: `No results found for ${param}`});
-}
+// TO handle any non-existing endpoints
+app.use((req,res) => {
+    res.status(404).json({error: 'Endpoint does not exist'});
+});
